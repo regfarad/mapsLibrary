ede98127acb93b917b23fdd7ac532349a4b5e825
regfarad <regfaradpro@gmail.com>
2016-10-19 20:55:23 +0200

    projet de thom
    projet de thom

---
+++ GoogleMapProjetTway/app/src/main/java/Modules/Duration.java
@@ -0,0 +1,14 @@
+package Modules;
+
+/**
+ * Created by Thom 16/10/2016.
+ */
+public class Duration {
+    public String text;
+    public int value;
+
+    public Duration(String text, int value) {
+        this.text = text;
+        this.value = value;
+    }
+}
