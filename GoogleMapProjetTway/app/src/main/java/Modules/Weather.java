304cc817f9b24e5302dddb07e7c3a78ba1ce3f38
TeamWorkerHELB3 <thomwabs91@hotmail.fr>
2016-12-16 12:32:42 +0100

    ok
    ok

--- GoogleMapProjetTway/app/src/main/java/Modules/Weather.java
+++ GoogleMapProjetTway/app/src/main/java/Modules/Weather.java
@@ -12,7 +12,7 @@ public class Weather {
     public Temperature temperature = new Temperature();
     public Wind wind = new Wind();
     public Rain rain = new Rain();
-    public Snow snow = new Snow()	;
+    public Snow snow = new Snow();
     public Clouds clouds = new Clouds();
 
     public byte[] iconData;
